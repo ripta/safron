af8f5b20b6e054393f61de214fa9ee7eae3ba4c4
Ripta Pasay <ripta@users.noreply.github.com>
2016-02-26 17:27:18 -0800

    safron: support for multiple log output formats

--- safron.go
+++ safron.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"log"
+	log "github.com/Sirupsen/logrus"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -14,6 +14,7 @@ var (
 	relRoot    = flag.String("path", ".", "Document root")
 	listenPort = flag.Int("port", 8080, "Port")
 	listenHost = flag.String("host", "0.0.0.0", "Host")
+	logFormat  = flag.String("log-format", "text", "Log format: text, json")
 	quiet      = flag.Bool("quiet", false, "No banner on startup")
 )
 
@@ -23,34 +24,47 @@ func withLogging(l *log.Logger, h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
 		h.ServeHTTP(w, r)
-		l.Printf("%s %s (%v)\n", r.Method, r.URL, time.Since(startTime))
+		l.WithFields(log.Fields{
+			"method": r.Method,
+			"path": r.URL.Path,
+			"duration": time.Since(startTime),
+		}).Info()
 	}
 }
 
 func main() {
-	logger := log.New(os.Stderr, "", log.LstdFlags)
 	flag.Parse()
+
+	logger := &log.Logger{
+		Out:   os.Stderr,
+		Level: log.InfoLevel,
+	}
+	switch *logFormat {
+	case "json":
+		logger.Formatter = new(log.JSONFormatter)
+	default:
+		logger.Formatter = new(log.TextFormatter)
+	}
 
 	absRoot, err := filepath.Abs(*relRoot)
 	if err != nil {
-		logger.Fatalf("%v\n", err)
+		logger.Fatalf("%v", err)
 	}
 
 	if _, err = os.Stat(absRoot); err != nil {
-		logger.Fatalf("%v\n", err)
+		logger.Fatalf("%v", err)
 	}
 
 	if !*quiet {
-		logger.Printf("Safron version %d\n", Version)
-		logger.Printf("Listening to http://%s:%d\n", *listenHost, *listenPort)
-		logger.Printf("Serving %s\n", absRoot)
-		logger.Printf("^C to exit\n")
+		logger.WithField("type", "banner").Infof("Safron version %d", Version)
+		logger.WithField("type", "banner").Infof("Listening to http://%s:%d", *listenHost, *listenPort)
+		logger.WithField("type", "banner").Infof("Serving %s", absRoot)
 	}
 
 	listen := *listenHost + ":" + strconv.Itoa(*listenPort)
 	handler := withLogging(logger, http.FileServer(http.Dir(absRoot)))
 
 	if err = http.ListenAndServe(listen, handler); err != nil {
-		logger.Fatalf("%v\n", err)
+		logger.Fatalf("%v", err)
 	}
 }
